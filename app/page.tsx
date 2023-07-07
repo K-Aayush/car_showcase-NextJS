27a5460d0bad064a9585578fceb7759a4049efb2
Aayush <ayushkarkee6@gmail.com>
2023-07-07 17:15:52 +0545

    Search manufacturer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Hero } from '@/components'
+import { Hero, CustomFilter, SearchBar } from '@/components'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home_text-container">
+          <h1 className="text-4xl font-extrabold">Car Catelogue</h1>
+          <p>Explore the cars you might like.</p>
+        </div>
+
+        <div className="home_filters">
+            <SearchBar />
+          <div className="home_filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="Year" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from "./hero";
 import CustomButton from "./customButton";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import CustomFilter from "./customFilter";
+import SearchBar from "./searchBar";
+import SearchManufacturer from "./searchManufacturer";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
