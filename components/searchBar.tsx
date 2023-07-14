b6938547aeb57a394f55e709e4a8733fa1403687
Aayush <ayushkarkee6@gmail.com>
2023-07-14 14:44:29 +0545

    update search params

--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { useState } from 'react'
 import { SearchManufacturer } from './'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
@@ -21,10 +22,37 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('');
     const [model, setModel] = useState('');
+    const router = useRouter();
 
-    const handleSearch = () => {
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
+      if(manufacturer === '' && model === '') {
+        return alert('Please Fill in the search bar')
+      }
+
+      updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
     }
+
+const updateSearchParams = (model: string, manufacturer: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  if(model) {
+    searchParams.set('model', model);
+  } else {
+    searchParams.delete('model');
+  }
+
+  if(manufacturer) {
+    searchParams.set('manufacturer', manufacturer);
+  } else {
+    searchParams.delete('manufacturer');
+  }
+
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+  router.push(newPathName);
+}
 
   return (
     <form className="searchbar" onSubmit={handleSearch} action="">
